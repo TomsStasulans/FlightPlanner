16442cacc503dc77b7c792c46cebbc048f7bd899
Toms <toms.stasulans@gmail.com>
2022-02-10 11:33:01 +0200

    Tests green.

---
+++ FlightPlanner/Controllers/CustomerApiController.cs
@@ -0,0 +1,46 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CustomerApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("airports")]
+        public IActionResult SearchAirports(string search)
+        {
+            var airports = FlightStorage.SearchAirport(search);
+            return Ok(airports);
+        }
+
+        [HttpPost]
+        [Route("flights/search")]
+        public IActionResult SearchFlights(SearchFlightRequest request)
+        {
+            if (request.From == request.To)
+            {
+                return BadRequest();
+            }
+
+            var page = new PageResult();
+            page.TotalItems++;
+            return Ok(page);
+        }
+
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult FindFlightById(int id)
+        {
+            var flight = FlightStorage.GetFlight(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flight);
+        }
+    }
+}
