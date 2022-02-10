205a80c75e68e6aaddf7c248f358ced637746d6f
Toms <toms.stasulans@gmail.com>
2022-02-10 11:24:52 +0200

    Finaly done.

---
+++ FlightPlanner/Models/PageResult.cs
@@ -0,0 +1,14 @@
+﻿namespace FlightPlanner.Models
+{
+    public class PageResult
+    {
+        public int Page { get; set; }
+        public int TotalItems { get; set; }
+        public List<Flight> Items { get; set; }
+
+        public PageResult()
+        {
+            Items = new List<Flight>();
+        }
+    }
+}
