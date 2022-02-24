192fc29eb49af30e41aee0b7f7dcc09f21a2974b
Toms <toms.stasulans@gmail.com>
2022-02-24 10:33:37 +0200

    files from 1-iteration pushed

--- FlightPlanner/Models/Airport.cs
+++ FlightPlanner/Models/Airport.cs
@@ -4,7 +4,6 @@ namespace FlightPlanner.Models
 {
     public class Airport
     {
-        public int Id { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
         [JsonPropertyName("airport")]
---
+++ FlightPlanner/Models/PageResult.cs
@@ -0,0 +1,16 @@
+﻿namespace FlightPlanner.Models
+{
+    public class PageResult
+    {
+        public int Page { get; set; }
+        public int TotalItems { get; set; }
+        public List<Flight> Items { get; set; }
+
+        public PageResult(List<Flight> input)
+        {
+            Page = 0;
+            TotalItems = input.Count;
+            Items = input;
+        }
+    }
+}
