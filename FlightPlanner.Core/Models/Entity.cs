e5a2bb4cceb24770e7628b6169805903dc2c1dd4
Toms <toms.stasulans@gmail.com>
2022-03-03 10:03:59 +0200

    Making models and interfaces.

--- FlightPlanner.Core/Models/Entity.cs
+++ FlightPlanner.Core/Models/Entity.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FlightPlanner.Core.Models
+﻿namespace FlightPlanner.Core.Models
 {
-    internal class Entity
+    public abstract class Entity
     {
+        public int Id { get; set; }
     }
 }
