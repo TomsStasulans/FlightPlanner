640f6275a2fcc017d6f04c83b9c2f37ec347be57
Edgars Salmiņš <edgars.salmins@if.lv>
2022-03-08 11:23:25 +0200

    startup fix

--- FlightPlanner/Program.cs
+++ FlightPlanner/Program.cs
@@ -1,5 +1,52 @@
-using FlightPlanner;
+using FlightPlanner.Core.Services;
+using FlightPlanner.Data;
+using FlightPlanner.Handlers;
+using FlightPlanner.Models;
+using FlightPlanner.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
 
-var app = Startup.InitializeApp(args);
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<FlightPlannerDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("flight-planner"));
+});
+
+builder.Services.AddRazorPages();
+builder.Services.AddTransient<IFlightPlannerDbContext, FlightPlannerDbContext>();
+builder.Services.AddTransient<IDbService, DbService>();
+builder.Services.AddTransient<IEntityService<Flight>, EntityService<Flight>>();
+builder.Services.AddTransient<IEntityService<Airport>, EntityService<Airport>>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
